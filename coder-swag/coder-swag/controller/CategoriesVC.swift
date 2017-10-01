751ebd1a97c027ddec52acde4dafb7afa632357e
Jess Rascal <jess@justonejess.com>
2017-10-01 07:09:24 +0100

    Lesson 1 complete

---
+++ coder-swag/coder-swag/controller/CategoriesVC.swift
@@ -0,0 +1,19 @@
+//
+//  CategoriesVC.swift
+//  coder-swag
+//
+//  Created by Jess Rascal on 01/10/2017.
+//  Copyright © 2017 jessrascal. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
