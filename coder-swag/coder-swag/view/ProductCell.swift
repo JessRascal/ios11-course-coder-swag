03b703c721161bfad30b8c9be0e576b0a2c30c12
Jess Rascal <jess@justonejess.com>
2017-10-04 20:31:09 +0100

    Lesson 3 complete

---
+++ coder-swag/coder-swag/view/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Jess Rascal on 04/10/2017.
+//  Copyright © 2017 jessrascal. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
